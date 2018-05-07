ea46b41fc8ee2f0eb3721aa4a92b869789122d2c
Minsoo <viviennes7@naver.com>
2018-05-07 14:23:45 +0900

    builder 패턴으로 변경

--- src/main/java/http/HttpResponse.java
+++ src/main/java/http/HttpResponse.java
@@ -3,42 +3,55 @@ package http;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
-import lombok.Getter;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Data
-@Getter
 public class HttpResponse <T>{
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private final int code;
+    private final T body;
+    private final Map<String, String> headers;
 
-    private int code;
-    private T body;
-    private Map<String, String> headers;
-
-    public HttpResponse(int code) {
-        this.code = code;
+    private HttpResponse(Builder<T> builder) {
+        this.code = builder.code;
+        this.body = builder.body;
+        this.headers = builder.headers;
     }
 
-    public HttpResponse(int code, String body) {
-        this(code);
-        this.body = this.stringToClass(body);
-    }
+    static public class Builder <T>{
+        private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public HttpResponse(int code, String body, Map<String, String> headers) {
-        this(code, body);
-        this.headers = headers;
-    }
+        private final int code;
+        private T body;
+        private Map<String, String> headers;
 
-    private T stringToClass(String string) {
-        T t;
-        try {
-            t = OBJECT_MAPPER.readValue(string, new TypeReference<T>() {});
-        } catch (IOException e) {
-            throw new IllegalStateException("ObjectMapper에서 String을 Object로 변환 중 오류가 발생했습니다.");
+        public Builder(int code) {
+            this.code = code;
         }
-        return t;
-    }
 
+        public Builder body(String body) {
+            this.body = this.stringToClass(body);
+            return this;
+        }
+
+        public Builder headers(Map<String, String> headers) {
+            this.headers = headers;
+            return this;
+        }
+
+        public HttpResponse<T> build() {
+            return new HttpResponse<>(this);
+        }
+
+        private T stringToClass(String string) {
+            T t;
+            try {
+                t = OBJECT_MAPPER.readValue(string, new TypeReference<T>() {});
+            } catch (IOException e) {
+                throw new IllegalStateException("ObjectMapper에서 String을 Object로 변환 중 오류가 발생했습니다.");
+            }
+            return t;
+        }
+    }
 }
